154bda0a358a0cf55277e3475ea3124a096628fc
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-03-24 13:21:03 +0700

    Initialized html and javascript

---
+++ index.js
@@ -0,0 +1,26 @@
+//store contact as object
+class contact {
+  constructor(id, name, phone, birthday, email, website, address) {
+    this.id = id
+    this.name = name
+    this.phone = phone
+    this.birthday = birthday
+    this.email = email
+    this.website = website
+    this.address = address
+  }
+}
+
+
+
+const newContact = () => {
+  const lastID = () => {
+    if()
+  }
+  const newName = document.getElementById("new-contact-name").value
+  const newPhone = document.getElementById("new-contact-phone").value
+  const newBirthday = document.getElementById("new-contact-birthday").value
+  const newEmail = document.getElementById('new-contact-email').value
+  const newWebsite = document.getElementById('new-contact-website').value
+  const newAddress = document.getElementById('new-contact-address').value
+}
