1410e7fdc35f47554093e174695076def0a90779
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-03-24 13:50:50 +0700

    Handle add new contact to array

--- index.js
+++ index.js
@@ -11,16 +11,39 @@ class contact {
   }
 }
 
+let contacts = []
 
-
-const newContact = () => {
-  const lastID = () => {
-    if()
+const newContact = event => {
+  event.preventDefault()
+  const calculateNewID = () => {
+    if (contacts.length === 0) {
+      firstID = 1
+      return firstID
+    }
+    const lastNum = contacts
+      .map(contact => contact.id)
+      .reduce((max, value) => Math.max(max, value))
+    return lastNum + 1
   }
+  const newID = calculateNewID()
   const newName = document.getElementById("new-contact-name").value
   const newPhone = document.getElementById("new-contact-phone").value
   const newBirthday = document.getElementById("new-contact-birthday").value
-  const newEmail = document.getElementById('new-contact-email').value
-  const newWebsite = document.getElementById('new-contact-website').value
-  const newAddress = document.getElementById('new-contact-address').value
+  const newEmail = document.getElementById("new-contact-email").value
+  const newWebsite = document.getElementById("new-contact-website").value
+  const newAddress = document.getElementById("new-contact-address").value
+
+  const newEntry = new contact(
+    newID,
+    newName,
+    newPhone,
+    newBirthday,
+    newEmail,
+    newWebsite,
+    newAddress
+  )
+  contacts.push(newEntry)
 }
+
+const form = document.getElementById("contact-form")
+form.addEventListener("submit", newContact)
